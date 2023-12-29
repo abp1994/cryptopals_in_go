458c7eb17aa6e354356ddfea7de55abd7ae063cd
Akaash <akaash.b.p@gmail.com>
2023-12-29 13:31:06 +0000

    new pkg

---
+++ pkg/utils/byteOperations.go
@@ -0,0 +1,19 @@
+package utils
+
+import "errors"
+
+func xorBytes(a, b []byte) ([]byte, error) {
+	if len(a) != len(b) {
+		return nil, errors.New("Length of Byte slices not equal!")
+	}
+
+	// Create a new byte slice to store the result
+	result := make([]byte, len(a))
+
+	// Iterate through each pair of bytes and apply XOR
+	for i := 0; i < len(a); i++ {
+		result[i] = a[i] ^ b[i]
+	}
+
+	return result, nil
+}
